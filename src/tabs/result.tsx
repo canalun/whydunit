70be75bba0e5b308c6f4fb4966c3992246f4fd73
canalun <i.am.kanaru.sato@gmail.com>
2024-07-29 00:34:02 +0900

    impl table

--- src/tabs/result.tsx
+++ src/tabs/result.tsx
@@ -15,24 +15,58 @@ function ResultPage() {
         }
       }) => {
         setData(newValue)
+        console.log(newValue[0].stack)
       }
     )
-  }, [])
+  }, [setData])
   return (
-    <table>
-      <tbody>
-        {data.map(({ name, stack, args, boundThis }, i) => {
-          return (
-            <tr>
-              <td key={i}>
-                {name}, {stack.split("\n").slice(2)}, {args}, {boundThis ?? ""}
-              </td>
-            </tr>
-          )
-        })}
-      </tbody>
-    </table>
+    <>
+      <button onClick={() => chrome.storage.local.remove("detected")}>
+        reset the result
+      </button>
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">API</th>
+            <th scope="col">Stack</th>
+            <th scope="col">Args</th>
+            <th scope="col">BoundThis</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data?.map(({ name, stack, args, boundThis }, i) => {
+            return (
+              <tr key={i}>
+                <td key={i}>{name}</td>
+                <td style={tableCellStyle(500)}>
+                  {...stack
+                    .split("\n")
+                    .slice(2)
+                    .map((line) => {
+                      return (
+                        <span>
+                          {line}
+                          <br />
+                        </span>
+                      )
+                    })}
+                </td>
+                <td style={tableCellStyle(500)}>{args}</td>
+                <td>{boundThis ?? ""}</td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+    </>
   )
 }
+
+const tableCellStyle = (maxWidth: number) => ({
+  whiteSpace: "nowrap",
+  maxWidth: maxWidth,
+  overflow: "hidden",
+  textOverflow: "ellipsis"
+})
 
 export default ResultPage
